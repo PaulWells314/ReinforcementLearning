5beab06baa6265ebdb06a2a38810d0d1aa9448d2
PaulWells314 <pwells314@yahoo.com>
2018-09-03 12:01:57 -0400

    Put actions in enum and add some function headers

--- LunarLander_RL.py
+++ LunarLander_RL.py
@@ -1,18 +1,32 @@
-import numpy as np
+"""
+Lunar Lander: An example of Reinforcement Learning by solving Bellman equations.
+"""
 
-NUM_POS     = 16   # 0 = ground  16 = maximum altitude
-NUM_VEL     = 32   
-NUM_ACTIONS = 2     # 0 = no action (gravity only)  1 = Thrust
-ZERO_VEL    = 16    # V > ZERO_VEL is up
-NUM_STEPS   = 128
-NUM_TRIALS  = 32
-DELTA       = 1
+import numpy as np
+from enum import Enum
+
+class Action(Enum):
+    ACTION_NONE = 1  # free-fall
+    ACTION_BURN = 2  # positve accelertation (up)
+
+
+NUM_POS         = 16    # 0 = ground  NUM_POS - 1 = maximum altitude
+NUM_VEL         = 32   
+ZERO_VEL        = 16    # vel > ZERO_VEL is up
+NUM_STEPS       = 128
+NUM_TRIALS      = 32
+DELTA           = 1
+DISCOUNT_FACTOR = 0.9
 
 def next_state(pos, vel, action):
+    """ State consists of discretized position and velocity.
+    This function generates the next state from the current position, velocity
+    and action.
+    """
     next_pos = pos + (vel - ZERO_VEL) * DELTA
     if next_pos >= NUM_POS:
         return (pos, vel)
-    if action == 1:
+    if action == Action.ACTION_BURN:
         acc = 2
     else:
         acc = -1
@@ -23,26 +37,25 @@ def next_state(pos, vel, action):
     
     
 def dumpV(V):
-        for pos in range(NUM_POS):
-            for vel in range(NUM_VEL):
-                if V[pos,vel] != 0.0:
-                    print("pos ", pos, " vel ", vel, " V ", V[pos,vel])
+    """ Dump Value matrix"""
+    for pos in range(NUM_POS):
+        for vel in range(NUM_VEL):
+            if V[pos,vel] != 0.0:
+                print("pos ", pos, " vel ", vel, " V ", V[pos,vel])
                     
 def main():
 
     V = np.zeros( (NUM_POS, NUM_VEL) )
     VTemp = np.zeros( (NUM_POS, NUM_VEL) )
-    R = np.zeros( (NUM_POS, NUM_VEL) )
-
-    
-   
+    R = np.zeros( (NUM_POS, NUM_VEL) ) 
     R[1:, :] =  0
    
     
     # Land as softly as possible
     for v in range(NUM_VEL):
-        R[0, v] = 5 * (v - ZERO_VEL)
+        R[0, v] =  5 * (v - ZERO_VEL)
     
+    # Generate Value matrix by iteration over Bellman equations
     for epoch in range(NUM_TRIALS):
         pos = NUM_POS -1
         vel = ZERO_VEL
@@ -54,7 +67,7 @@ def main():
                 best_pos =    pos
                 best_vel =    vel
                 
-                for j in range(NUM_ACTIONS):
+                for j in Action:
                     test_pos, test_vel = next_state(pos, vel, j)
                     if V[test_pos, test_vel] > best_V:     
                         best_action = j
@@ -63,18 +76,18 @@ def main():
                         best_V = V[test_pos, test_vel]
                    
                         
-                VTemp[pos, vel] = R[pos, vel] + 0.9 * best_V
+                VTemp[pos, vel] = R[pos, vel] + DISCOUNT_FACTOR * best_V
         V = VTemp
         #print("iteration ", epoch) 
         #dumpV(V)
         #print("")
         
-    # Best path from given position
-    pos = 15
+    # Determine best path from given position
+    pos = NUM_POS - 1
     vel = ZERO_VEL
     best_V = -10000
     while (pos > 0):
-        for j in range(NUM_ACTIONS):
+        for j in Action:
             test_pos, test_vel = next_state(pos, vel, j)
             if V[test_pos, test_vel] > best_V:     
                 best_action = j
